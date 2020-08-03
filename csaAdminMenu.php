9c444c4a247fc3ec8891a4225203c00587c5ab5b
Hafiz <hafiz19060@gmail.com>
2020-08-03 12:49:45 +0800

    admin dashboard/menu part 1

---
+++ csaAdminMenu.php
@@ -0,0 +1,10 @@
+<html>
+
+
+<p><a href="Login/csaLogout.php">Log Out</a></p>
+<hr>
+<a href = "csaAdminProfile.php">View profile</a>
+<a href = "csaRegCashier.php">Register Cashier</a><br>
+<br>
+
+</html>
