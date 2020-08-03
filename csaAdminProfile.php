4079a933ac18b9b4026df59e8b1cf296687f6041
Hafiz <hafiz19060@gmail.com>
2020-08-03 12:50:35 +0800

    admin profile part 1. x connect db lagi

---
+++ csaAdminProfile.php
@@ -0,0 +1,23 @@
+<html>
+
+<a href = "csaAdminMenu.php">Return to menu</a><br><hr>
+Username:<br>
+<input type = "text" name = "adminUname"><br>
+Password:<br>
+<input type = "text" name = "adminPassword"><br>
+Name:<br>
+<input type = "text" name = "adminName"><br>
+Mobile Number:<br>
+<input type = "text" name = "adminMobileNum"><br>
+IC Number:<br>
+<input type = "text" name = "adminICNum"><br>
+
+
+
+
+
+
+
+
+
+</html>
