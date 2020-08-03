eb5150696b2bb5e980e4fc2b3be0d3c9122672c1
Hafiz <hafiz19060@gmail.com>
2020-08-03 11:13:24 +0800

    Login page w/o css

---
+++ Login/csaLogin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+ <form action="checkLogin.php" method="post">
+      
+ 
+    <label for="uname"><b>Username:</b></label>
+    <input type="text" placeholder="Enter Your ID" name="id"  > <br>
+
+    <label for="psw"><b>Password:</b></label>
+    <input type="password" placeholder="Enter Your Password" name="psw" > <br><br>
+
+    <button type="submit">Login</button>
+
+</form>
