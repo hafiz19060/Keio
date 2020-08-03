54024bcd132090a4d137b0eea7ec40a8cceeb3f0
Hafiz <hafiz19060@gmail.com>
2020-08-03 12:51:19 +0800

    admin register cashier form w/o css

---
+++ csaRegCashier.php
@@ -0,0 +1,19 @@
+<html>
+<a href = "csaAdminMenu.php">Back to menu</a><br><hr>
+<form action = "process.php" method = "post">
+Username:<br>
+<input type = "text" name = "cashierUname"><br>
+Password:<br>
+<input type = "text" name = "cashierPassword"><br>
+Name:<br>
+<input type = "text" name = "cashierName"><br>
+Mobile number:<br>
+<input type = "text" name = "cashierMobileNum"><br>
+IC number:<br>
+<input type = "text" name = "cashierICNum"><br><br>
+
+<input type = "submit" name = "regCashier">
+
+</form>
+
+</html>
